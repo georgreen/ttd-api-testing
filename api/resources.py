2fb8b6f53c4cdab3c006e21042146dea18cce583
georgreen <geogreenmanu@gmail.com>
2018-08-20 12:55:24 +0300

    Test with no feature Implementation.

--- api/resources.py
+++ api/resources.py
@@ -1,5 +1,19 @@
 from flask_restful import Resource
+from flask import request
 
 class PhoneBookResource(Resource):
     def get(self):
-        return {"message": "hello from phone book"}
+        """
+        Get all contacts
+
+        params: self
+
+        returns: {"message": "success", [{"georgreen": 254709845454}]}
+        """
+        user_name = request.args.get('user_name')
+
+
+        if type(user_name) != str:
+            return {"message": "Bad data"}, 400
+
+        return {"message": "hello from phone book"}, 200
--- tests/test_baseTestCase.py
+++ tests/test_baseTestCase.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from app import create_app
 import os
+import json
 
 class BaseTestCase(TestCase):
 
@@ -10,11 +11,49 @@ class BaseTestCase(TestCase):
         self.app = create_app("Testing")
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.test_client = self.app.test_client()
 
     def tearDown(self):
         self.app_context.pop()
 
 
 class PhoneBookTestCase(BaseTestCase):
-    def test_get_contact(self):
-        pass
+    def test_get_contact_unexpected_input(self):
+        """
+        Example: {}, []
+        """
+        results = self.test_client.get('/api/v1/phonebook?user_name=[]')
+        results_payload = json.loads(results.data)
+
+        self.assertEqual(results.status_code, 400)
+        self.assertEqual(results_payload['message'], 'Bad data')
+
+    def test_get_contact_normal(self):
+        """
+        Example: "normal"
+        """
+        results = self.test_client.get('/api/v1/phonebook?user_name=normal')
+        results_payload = json.loads(results.data)
+
+        self.assertEqual(results.status_code, 200)
+        self.assertEqual(results_payload['message'], 'Successfully retrieved')
+
+    def test_get_contact_unexpected_boundary(self):
+        """
+        Example: '', ' '
+        """
+        results = self.test_client.get('/api/v1/phonebook?user_name=""')
+        results_payload = json.loads(results.data)
+
+        self.assertEqual(results.status_code, 400)
+        self.assertEqual(results_payload['message'], 'Bad data')
+
+    def test_get_contact_unexpected_edgecase(self):
+        """
+        Example: "      ", "   ?", " '' "
+        """
+        results = self.test_client.get('/api/v1/phonebook?user_name="  ?"')
+        results_payload = json.loads(results.data)
+
+        self.assertEqual(results.status_code, 400)
+        self.assertEqual(results_payload['message'], 'Bad data')
